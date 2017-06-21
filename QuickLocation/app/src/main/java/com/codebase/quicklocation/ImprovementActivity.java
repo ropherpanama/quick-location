120331670ee1acf0a2b338ceca229a19e9b88d65
Rosendo <Rosendo>
2017-06-21 00:48:55 -0500

    Se agrega la trama de mejora de datos desde el formulario

--- QuickLocation/app/src/main/java/com/codebase/quicklocation/ImprovementActivity.java
+++ QuickLocation/app/src/main/java/com/codebase/quicklocation/ImprovementActivity.java
@@ -8,7 +8,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.codebase.quicklocation.database.Users;
@@ -17,6 +20,7 @@ import com.codebase.quicklocation.model.ImprovementInformation;
 import com.codebase.quicklocation.model.ImprovementRequest;
 import com.codebase.quicklocation.model.LastLocation;
 import com.codebase.quicklocation.model.Location;
+import com.codebase.quicklocation.model.Schedule;
 import com.codebase.quicklocation.utils.Reporter;
 import com.codebase.quicklocation.utils.Utils;
 
@@ -30,6 +34,15 @@ public class ImprovementActivity extends AppCompatActivity {
     private String locationData = "";
     private boolean shouldSendData = false;
     private String placeId;
+    private Spinner spinnerFrom;
+    private Spinner spinnerTo;
+    private Spinner spinnerSatFrom;
+    private Spinner spinnerSatTo;
+    private Spinner spinnerSunFrom;
+    private Spinner spinnerSunTo;
+    private CheckBox closedSunday;
+    private CheckBox closedWeekend;
+    private String [] weekdays = {"LU", "MA", "MI", "JU", "VI"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +55,54 @@ public class ImprovementActivity extends AppCompatActivity {
         telefono = (EditText) findViewById(R.id.edit_new_phone);
         Bundle bundle = getIntent().getExtras();
         placeId = bundle.getString("place_id");
+        closedSunday  = (CheckBox) findViewById(R.id.checkboxSundayClosed);
+        closedWeekend = (CheckBox) findViewById(R.id.checkboxWeekendClosed);
+
+        //Adapter para todos los spinners
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.schedule_hours, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFrom = (Spinner) findViewById(R.id.spinner_from);
+        spinnerFrom.setAdapter(adapter);
+        spinnerTo = (Spinner) findViewById(R.id.spinner_to);
+        spinnerTo.setAdapter(adapter);
+        spinnerSatFrom = (Spinner) findViewById(R.id.spinner_from_sat);
+        spinnerSatFrom.setAdapter(adapter);
+        spinnerSatTo = (Spinner) findViewById(R.id.spinner_to_sat);
+        spinnerSatTo.setAdapter(adapter);
+        spinnerSunFrom = (Spinner) findViewById(R.id.spinner_from_sun);
+        spinnerSunFrom.setAdapter(adapter);
+        spinnerSunTo = (Spinner) findViewById(R.id.spinner_to_sun);
+        spinnerSunTo.setAdapter(adapter);
+
+        closedSunday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((CheckBox) v).isChecked()) {
+                    spinnerSunFrom.setEnabled(false);
+                    spinnerSunTo.setEnabled(false);
+                } else {
+                    spinnerSunFrom.setEnabled(true);
+                    spinnerSunTo.setEnabled(true);
+                }
+            }
+        });
+
+        closedWeekend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox) v).isChecked()) {
+                    spinnerSatFrom.setEnabled(false);
+                    spinnerSatTo.setEnabled(false);
+                    spinnerSunFrom.setEnabled(false);
+                    spinnerSunTo.setEnabled(false);
+                } else {
+                    spinnerSatFrom.setEnabled(true);
+                    spinnerSatTo.setEnabled(true);
+                    spinnerSunFrom.setEnabled(true);
+                    spinnerSunTo.setEnabled(true);
+                }
+            }
+        });
     }
 
     @Override
@@ -105,11 +166,44 @@ public class ImprovementActivity extends AppCompatActivity {
                 shouldSendData = true;
             }
 
+            ImprovementInformation i = new ImprovementInformation();
+            i.setInformationTag("schedule");
+            i.setSchedule(true);
+
+            List<Schedule> schedules = new ArrayList<>();
+            //Se agrega lunes a viernes
+            String hourFrom = spinnerFrom.getSelectedItem().toString();
+            String hourTo = spinnerTo.getSelectedItem().toString();
+
+            for(String s : weekdays) {
+                schedules.add(new Schedule(s, hourFrom, hourTo, true));
+            }
+            //se agregan los fines de semana
+            if(closedWeekend.isChecked()) {
+                schedules.add(new Schedule("SA", "", "", false));
+            } else {
+                schedules.add(new Schedule("SA", spinnerSatFrom.getSelectedItem().toString(), spinnerSatTo.getSelectedItem().toString(), true));
+            }
+
+            if(!closedSunday.isChecked() && !closedWeekend.isChecked()) {
+                schedules.add(new Schedule("DO", spinnerSunFrom.getSelectedItem().toString(), spinnerSunTo.getSelectedItem().toString(), true));
+            } else {
+                schedules.add(new Schedule("DO", "", "", false));
+            }
+
+            i.setSchedules(schedules);
+            informations.add(i);
+
             request.setInformations(informations);
+
+            //Se valida que se ingreso algo en los horarios
+            if(!schedules.isEmpty())
+                shouldSendData = true;
 
             if(shouldSendData) {
                 //enviar trama al servidor
                 System.out.println(Utils.objectToJson(request));
+                finish();
             } else {
                 Snackbar.make(toolbar, "No se ha ingresado nueva data", Snackbar.LENGTH_SHORT).show();
             }
