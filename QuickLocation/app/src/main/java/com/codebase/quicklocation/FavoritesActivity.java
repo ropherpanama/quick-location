7b5b8defde17ae02560106f5a7034f58b2aeb5f1
Alexander Urriola <alexx198906@gmail.com>
2017-07-02 19:52:00 -0500

    valido null
    valido que el intent “add_favorite” exista!

--- QuickLocation/app/src/main/java/com/codebase/quicklocation/FavoritesActivity.java
+++ QuickLocation/app/src/main/java/com/codebase/quicklocation/FavoritesActivity.java
@@ -37,7 +37,9 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
-        add_favorite = getIntent().getExtras().getBoolean("add_favorite");
+        if (getIntent().hasExtra("add_favorite")) {
+            add_favorite = getIntent().getExtras().getBoolean("add_favorite");
+        }
         try {
             toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
